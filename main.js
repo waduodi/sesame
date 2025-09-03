72341167ed46fbd222af33d197891ce86e4f42e4
30226959 <arwa.almousawi@ucalgary.ca>
2025-09-03 00:58:20 -0600

    added favicon, messed w/ sizing for landing page, added hover effects, filled in lost .gitignore info

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ const path = require("path");
 
 function createWindow() {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const aspectRatio = width / height;
+
   const win = new BrowserWindow({
     width,
     height,
-    resizable: false,
+    resizable: true, // allow resizing for ease of use (fullscreen kinda clunky)
     maximizable: false,
-    autoHideMenuBar: false,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "images", "favicon.ico"),
     webPreferences: {
       nodeIntegration: true
     }
+  });
+
+
+  // listener to maintain aspect ratio on resize
+  win.on("will-resize", (event, newBounds) => {
+    const newWidth = newBounds.width;
+    const newHeight = Math.round(newWidth / aspectRatio);
+    event.preventDefault();
+    win.setBounds({ width: newWidth, height: newHeight });
   });
 
   win.loadFile(path.join(__dirname, "index.html"));
